330ae4db3b701f3134fd5b5c30ae861b582de5af
jeeradate <jeeradate@gmail.com>
2024-11-21 11:03:52 +0700

    First

---
+++ src/lib/components/Effects.svelte
@@ -0,0 +1,25 @@
+<script>
+    let  elapsed = $state(0);
+    let interval = $state(1000);
+    let click = new Audio('click.mp3');
+
+    $effect(()=> {
+        const id = setInterval(()=>{
+            elapsed += 1;
+            if(elapsed >100){
+                elapsed = 0;
+                click.play();
+            }
+        },interval);
+        return()=>{
+            clearInterval(id);
+        };
+    });
+
+</script>
+
+<button onclick={() => interval /= 2}>speed up</button>
+<button onclick={() => interval *= 2}>slow down</button>
+
+<p>elapsed: {elapsed}</p>
+<p>interval: {interval}</p>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    let click = new Audio('click.mp3');
+    let count = $state(0);
+    import Effects from "$lib/components/Effects.svelte";
+    function onclick(){
+        click.play();
+        count++;
+        if(count >5){
+            count = 0;
+        }
+    }
+</script>
+
+
+<button {onclick}>
+    clikcs: {count}
+</button>
+<hr>
+<div style="padding: 30px;">
+    <h3>Effects Component</h3>
+    <Effects />
+</div>
